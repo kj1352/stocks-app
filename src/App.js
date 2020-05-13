b9ca68fb048441d2b26c6954cbb2d8ff57d16865
kj1352 <nikhilkj24@gmail.com>
2020-05-13 17:02:34 +0530

    Initial commit with basic UI setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/arrows.svg';
+import './css/home.css';
+import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function redirectUser(e) {
+
+        ReactDOM.render (
+            <div className="container">
+                <nav>
+                    <a href="/"> Home </a>
+                    <a className="active" onClick={redirectUser}> Stocks </a>
+                </nav>
+
+            </div>
+        , document.getElementById('root'));
+    }
+
+    redirectUser();
+
+    return (
+        <div className="container">
+            <nav>
+                <a href="/" className="active"> Home </a>
+                <a onClick={redirectUser}> Stocks </a>
+            </nav>
+            <div className="welcome">
+                <header> <h1> Stock Prices </h1> <img src={logo} className="App-logo" alt="logo" />  </header>
+                <p>
+                    Welcome to the Stock Market Page. You may click on stocks to view all the stocks or search to
+                    view the latest 100 days of activity.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
